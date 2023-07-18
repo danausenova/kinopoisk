ce81b33f41aaaf5708ad61c2290de044da0a092c
danausenova <aidana99969@gmail.com>
2023-07-18 16:54:45 +0600

    add movie

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,11 +1,31 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
+import { ACTION, API } from "../utils/consts";
+import axios from "axios";
 
 const movieContext = createContext();
 export function useMovieContext() {
   return useContext(movieContext);
 }
+const init = { movies: [] };
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTION.movies:
+      return { ...state, movies: action.payload };
+    default:
+      return state;
+  }
+}
 const MovieContext = ({ children }) => {
-  const value = {};
+  const { state, dispatch } = useReducer(reducer, init);
+
+  async function addMovie(obj) {
+    try {
+      await axios.post(API, obj);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  const value = { addMovie };
   return (
     <movieContext.Provider value={value}>{children}</movieContext.Provider>
   );
