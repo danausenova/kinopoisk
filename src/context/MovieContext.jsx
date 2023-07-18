ee30c97516c905e145d28060ccf2f8b5a062f020
JakaOmurzakova <angelina_18_89@mail.ru>
2023-07-18 17:30:53 +0600

    movieItem

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,11 +1,24 @@
+import axios from "axios";
 import React, { createContext, useContext } from "react";
+import { API } from "../utils/consts.js";
 
 const movieContext = createContext();
 export function useMovieContext() {
   return useContext(movieContext);
 }
+
+async function deleteMovie(id) {
+  try {
+    await axios.delete(`${API}/${id}`);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 const MovieContext = ({ children }) => {
-  const value = {};
+  const value = {
+    deleteMovie,
+  };
   return (
     <movieContext.Provider value={value}>{children}</movieContext.Provider>
   );
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import MovieItem from "../components/MovieItem";
 
 const MainLayout = () => {
   return (
     <div>
+      <MovieItem />
       <Navbar />
       <Outlet />
     </div>
